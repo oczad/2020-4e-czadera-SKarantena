96e253defe2bb122682ccdc19a52368da048d9c2
ondrej.czadera@student.gyarab.cz <Kbhfen2369>
2021-03-21 15:30:43 +0100

    DB connection

---
+++ db.js
@@ -0,0 +1,17 @@
+const sql = require('mysql');
+
+const con = sql.createConnection({
+    host: "localhost",
+    password: "pass",
+    database: "mydb"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected");
+    const sql = "";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Done something");
+    });
+});
